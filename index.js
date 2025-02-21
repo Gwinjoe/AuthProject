d0ddaf1b80f10d5a4ab3e885559cce977ca7d0b4
gwinjoe <godfreyjn873@gmail.com>
2025-02-21 19:46:35 +0100

    starter

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet")
+const cookieParser = require("cookie-parser")
+const app = express();
+const PORT = process.env.PORT || 3000 
+
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({mesage: "i am working"})
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on PORT ${PORT}`)
+})
