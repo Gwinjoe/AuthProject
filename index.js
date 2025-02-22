ecbe8688ce6ccec72eccfe2f5380e27de865bbc5
gwinjoe <godfreyjn873@gmail.com>
2025-02-23 00:34:45 +0100

    continuation

--- index.js
+++ index.js
@@ -3,19 +3,33 @@ const cors = require("cors");
 const helmet = require("helmet")
 const cookieParser = require("cookie-parser")
 const app = express();
+const mongoose = require("mongoose")
 const PORT = process.env.PORT || 3000 
-
+const router = require("./routers/authRouter")
+const path = require("path")
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("MongoDB Database Connected")
+}).catch((err) => {
+        console.error(err)
+    })
+
+
+app.use("/api/auth", router)
 app.get("/", (req, res) => {
     res.json({message: "i am working"})
 })
-
+app.get("/views", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+})
 
 
 app.listen(PORT, () => {
     console.log(`Server Listening on PORT ${PORT}`)
+    console.log(process.env.MONGO_URI)
 })
---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    email: {
+        required: [true, "email is required"],
+        type: String,
+        trim: true,
+        unique: [true, "email must be unique"],
+        minLength: [5, "email must be more than 5 characters"],
+        lowercase: true,
+    },
+    password: {
+        required: [true, "Password is required"],
+        select: false,
+        trim: true,
+        type: String,
+    },
+    verified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationCode: {
+        type: String,
+        select: false,
+    }
+    verificationCodeValidation: {
+        type: Number,
+        select: false,
+    },
+    forgotPasswordCode: {
+        type: String,
+        select: false,
+    },
+    forgotPasswordCodeValidation: {
+        type: Number,
+        select: false,
+    }
+}, {
+        timestamps: true,
+    })
+
+module.exports = mongoose.model("User", userSchema);
